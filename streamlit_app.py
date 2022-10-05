be3e261da174472c6e1348dc330190774e8f3fe8
Rodrigo <115064252+rcione-snowflake@users.noreply.github.com>
2022-10-05 17:43:59 +0200

    Update streamlit_app.py
    Add breakfast menu header and 4 items
    Add Build your own Smothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom´s New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🫐🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥤🥬Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🥪Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
